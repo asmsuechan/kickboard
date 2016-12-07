80847e661349cbff8fe62ce846a234160f39a515
asmsuechan <suenagaryoutaabc@gmail.com>
2016-12-08 00:07:51 +0900

    デフォルトのコミットメッセージを設定

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,10 +1,14 @@
 require Rails.root.join('script/shell_script_runner')
 
 class Attachment < ApplicationRecord
+  DEFAULT_COMMIT_MESSAGE = '[auto commit from kickboard]'.freeze
+
+  # TODO: アップロードするzipの制約等コメントに残す
   include FileUploader[:file]
   include ShellScriptRunner
 
   before_save :setup
+  before_save :set_message
   after_commit :unzip
   after_commit :commit
 
@@ -18,5 +22,9 @@ class Attachment < ApplicationRecord
 
   def setup
     ShellScriptRunner.setup
+  end
+
+  def set_message
+    self.message ||= DEFAULT_COMMIT_MESSAGE unless self.message
   end
 end
