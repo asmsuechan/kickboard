1ae1d363f56b2cf4f1635282c262a29065c526c8
asmsuechan <suenagaryoutaabc@gmail.com>
2016-12-08 11:40:07 +0900

    annotateを追加

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: attachments
+#
+#  id         :integer          not null, primary key
+#  file_data  :text
+#  message    :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  repo_name  :string           not null
+#
+
 require Rails.root.join('script/shell_script_runner')
 
 class Attachment < ApplicationRecord
